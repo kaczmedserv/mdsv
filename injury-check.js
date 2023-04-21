78b72fd41f75a0c47ab71588a6dfd79ed7a4c03a
kaczmedserv <kaczmedserv@gmail.com>
2023-04-21 10:42:32 +0200

    23.04.21

---
+++ injury-check.js
@@ -0,0 +1,53 @@
+// Injury Check Tool
+// @kacper.czapran@medserv.ie
+
+const url = `${window.location.origin}/claimforms/side1/open.php?procid=`;
+const pfl = `${window.location.origin}/backend/proceduresfromlist.php`;
+
+if (window.location.href == pfl) {
+    const DATA_IN = prompt("Paste Invoice Numbers / PROCIDs below.").split(/\r?\n/);
+    const DATA_OUT = [];
+
+    async function getData(dataIn) {
+        if (dataIn?.includes("-")) {
+            dataIn = parseInt(dataIn.split("-")[1]) - 1000;
+        }
+        return await fetch(url + dataIn)
+            .then((response) => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error(response.status);
+                }
+            })
+            .then((data) => {
+                const parser = new DOMParser();
+                const d = parser.parseFromString(data, "text/html");
+                try {
+                    if (d.getElementsByTagName('input')[17].checked) {
+                        return dataIn;
+                    } else {
+                        return 0;
+                    }
+                } catch (e) {}
+            })
+            .catch(error => {
+                console.error('Error!', error);
+            });
+    }
+
+    async function printResult() {
+        document.getElementById("procids").value = "Injury check in progress..."; 
+        for (let dataIn of DATA_IN) {
+            const r = await getData(dataIn);
+            if (r) {
+                DATA_OUT.push(r); 
+            }
+        }
+        document.getElementById("procids").value = DATA_OUT.join("\n");
+    }
+
+    printResult();
+} else {
+    alert("This tool works only in \"Review Procedures from list\"!");
+}
