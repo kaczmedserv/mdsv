ca829714980691d0061ea6d9482b8f304ce8307d
kaczmedserv <kaczmedserv@gmail.com>
2023-05-05 14:06:00 +0200

    23.05.05

--- codes-converter.js
+++ codes-converter.js
@@ -9,6 +9,7 @@ if (window.location.href.includes(bs)) {
         ["ORBIT","7041","IL22-62300011","IL22-62310011"],
         ["IAM","7041","IL22-62300021","IL22-62310021"],
         ["PITUITARY","7041","IL22-62300031","IL22-62310031"],
+        ["SINUS","7041","IL22-62300041","IL22-62310041"],
         ["BRAIN","7041","IL22-62300041","IL22-62310041"],
         ["CERVICAL","7046","IL22-62300411","IL22-62310411"],
         ["CSPINE","7046","IL22-62300411","IL22-62310411"],
@@ -25,7 +26,8 @@ if (window.location.href.includes(bs)) {
         ["WRIST","7079","IL22-62307121","IL22-62317121"],
         ["PELVIS","7047","IL22-62301011","IL22-62311011"],
         ["SIJ","7047","IL22-62301031","IL22-62311031"],
-        ["PROSTATE","198","IL22-62307251","IL22-62317251"],
+        ["HAND","7079","IL22-62301011","IL22-62311011"],
+        ["PROSTATE","7061","IL22-62307251","IL22-62317251"],
         ["SMALL BOWEL","7087","IL22-62307201","IL22-62317201"],
         ["ENTEROGRAPHY","7087","IL22-62307201","IL22-62317201"],
         ["MRCP","7057","IL22-62301601","IL22-62311601"],
@@ -33,27 +35,7 @@ if (window.location.href.includes(bs)) {
         ["FISTULA","7056","IL22-62301511","IL22-62311511"],
         ["ADRENAL","7056","IL22-62307151","IL22-62317151"],
         ["RENAL","7056","IL22-62307161","IL22-62317161"],
-        ["HAND","7047","IL22-62301011","IL22-62311011"],
-        ["THUMB","7047","IL22-62301011","IL22-62311011"],
-        ["HUMERUS","7047","IL22-62301011","IL22-62311011"],
-        ["TIBIA","7047","IL22-62301011","IL22-62311011"],
-        ["FIBULA","7047","IL22-62301011","IL22-62311011"],
-        ["THIGH","7047","IL22-62301011","IL22-62311011"],
-        ["FEMUR","7047","IL22-62301011","IL22-62311011"],
-        ["CALF","7047","IL22-62301011","IL22-62311011"],
-        ["SCAPULA","7047","IL22-62301011","IL22-62311011"],
-        ["TMJ","7047","IL22-62301011","IL22-62311011"],
-        ["FACIAL","7047","IL22-62301011","IL22-62311011"],
-        ["SACRUM","7047","IL22-62301011","IL22-62311011"],
-        ["COCCYX","7047","IL22-62301011","IL22-62311011"],
-        ["TIB","7047","IL22-62301011","IL22-62311011"],
-        ["FIB","7047","IL22-62301011","IL22-62311011"],
-        ["CLAVICLE","7047","IL22-62301011","IL22-62311011"],
-        ["FINGER","7047","IL22-62301011","IL22-62311011"],
-        ["CHEST","7047","IL22-62301011","IL22-62311011"],
-        ["THORAX","7047","IL22-62301011","IL22-62311011"],
-        ["SPLEEN","7047","IL22-62301011","IL22-62311011"],
-        ["MANDIBLE","7047","IL22-62301011","IL22-62311011"]
+        ["KIDNEY","7056","IL22-62307161","IL22-62317161"]
     ];
 
     const D = document.getElementsByTagName("tr");
@@ -76,14 +58,26 @@ if (window.location.href.includes(bs)) {
     } while (D[i].getElementsByTagName("td")[0].textContent);
 
     CONVERT_NEXT:
-    for (let description of DESCRIPTION) {
+    for (let index = 0; index < DESCRIPTION.length; index++) {
         for (let code of CODE_MATRIX) {
-            if (description?.toUpperCase().includes(code[0]) && !description?.toUpperCase().includes("CT")) {
-                if (description?.toUpperCase().includes("CONTRAST")) {
+            if (DESCRIPTION[index]?.toUpperCase().includes(code[0]) && !DESCRIPTION[index]?.toUpperCase().includes("CT")) {
+                if (DESCRIPTION[index]?.toUpperCase().includes("CONTRAST")) {
                     CODE_OUT.push(code[3]);  
                 } else {
                     CODE_OUT.push(code[2]);
                 }
+                continue CONVERT_NEXT;
+            }
+            if (CODE[index] == "22-7047") {
+                if (DESCRIPTION[index]?.toUpperCase().includes("CONTRAST")) {
+                    CODE_OUT.push("IL22-62301011");  
+                } else {
+                    CODE_OUT.push("IL22-62311011");
+                }
+                continue CONVERT_NEXT;
+            }
+            if (DESCRIPTION[index]?.toUpperCase().includes("CT") || DESCRIPTION[index]?.toUpperCase().includes("DEXA")) {
+                CODE_OUT.push(CODE[index]);
                 continue CONVERT_NEXT;
             }
         }
